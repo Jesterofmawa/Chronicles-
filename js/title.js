075e550dc281d1068bba524e42d5b80cf652d4fc
Jesterofmawa <teejaywiggins@hotmail.co.uk>
2026-08-07 18:21:21 +0100

    title and pip intro started

---
+++ js/title.js
@@ -0,0 +1,13 @@
+function beginGame() {
+
+    document.getElementById("titleScreen").style.display = "none";
+    document.getElementById("pipIntro").style.display = "block";
+
+}
+
+function beginAdventure() {
+
+    document.getElementById("pipIntro").style.display = "none";
+    document.getElementById("game").style.display = "block";
+
+}
